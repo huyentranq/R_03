7beb3d29bf6d22fb3e5e135f9f5cce0612529da3
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-03-16 11:03:29 +0700

    Update lab2_Bai3.R

--- lab2_Bai3.R
+++ lab2_Bai3.R
@@ -1,4 +1,47 @@
-read.csv('D:/R_lab/data01.csv',header=T)
+---------BAI 1------------------------
+sphere_vol <- function(r_vec){
+	v<-0
+	v<-round((4*pi*r_vec^3)/3,2)
+	mydf<-data.frame('Radius'=r_vec,'Volume'=v)
+	return (mydf)
+}
+sphere_vol(c(0.2,0.5,1,1.5))
+
+--------BAI 2----------------------------------
+
+x<-c(1,2,5,7,-3,0,5,1,5,6)
+y<-c(2,2,0,-5,7,8,11,9,3,2)
+#find even number in x
+u<-subset(x,x%%2==0) 
+v<-subset(y,y%%2==1)
+u
+v
+x[x>0] # Extract elements of x greater than 0
+y[y>0]
+trung_binh_mau<- mean(x)
+phuong_sai <- var(x) 
+do_lech_chuan <- sd(x)
+sort(x) #sort increase
+sort(x,T)#sort decrease
+
+
+-------BAI 2---------------------------------
+x<-c(1,2,5,7,-3,0,5,1,5,6)
+y<-c(2,2,0,-5,7,8,11,9,3,2)
+#find even number in x
+u<-subset(x,x%%2==0) 
+v<-subset(y,y%%2==1)
+u
+v
+x[x>0] # Extract elements of x greater than 0
+y[y>0]
+trung_binh_mau<- mean(x)
+phuong_sai <- var(x) 
+do_lech_chuan <- sd(x)
+sort(x) #sort increase
+sort(x,T)#sort decrease
+--------BAI 3-----------------------------------
+
 setwd('D:/R_lab')
 ex4_plants_height<-read.csv('data11.csv',header=T)
 ex4_plants_height
