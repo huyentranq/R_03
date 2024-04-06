ddf68d7b25deaeb4030f925d25c42128d6177446
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-04-06 09:12:29 +0700

    Update TH3.r

--- TH3.r
+++ TH3.r
@@ -14,3 +14,17 @@ par(mfrow = c(1,3)) ##chia bảng thành 3 phẦN
 legend("left",legend=labels,horiz=FALSE,fill=mycol, border="black",cex=1.5)
 
 
+---------------EX3---------------------------------------
+labels<-c("oil","coal","gas","renewal energies","nuclear energy","others")
+ rates<-c(33.1,27,21.1,12.8,5.8,0.2)
+ mycol <- c("lavender", "lightblue", "mistyrose", "cornsilk","orange3","lightcyan")
+ barplot(rates,names.arg = rep(1:6),
+    width = 2,
+    cex.name=1, 
+    col=mycol,
+    ylim=c(0,50),
+    border="darkgreen",
+    ylab="energy rates ",
+    main="ty trong energy sources")
+ par(mfrow = c(1,2)) 
+ legend("left",legend=labels,horiz=FALSE,fill=mycol, border="black",cex=1)
