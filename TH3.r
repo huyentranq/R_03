033bab90c834aeb0f642902382e18b05423c233a
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-04-06 08:42:46 +0700

    Create TH3.r

---
+++ TH3.r
@@ -0,0 +1,16 @@
+setwd('E:/R_lab')
+mydata_ex2<-read.csv('SmartPhoneMarket2022-2023.csv',header=T)
+labels<- mydata_ex2[,1];
+data_2022<-mydata_ex2[,2];
+ 
+data_2023<- mydata_ex2[,3]
+mycol<-c("white","green","purple","violet","brown","red")
+my_pie1<-pie(data_2023,labels,radius=1,col=mycol,init.angle=90,main='tỉ trọng năm 2023')
+my_pie2<-pie(data_2022,labels,radius=1,col=mycol,init.angle=90,main='tỉ trọng năm 2022')
+
+par(mfrow = c(1,3)) ##chia bảng thành 3 phẦN
+
+##tạo chú thích
+legend("left",legend=labels,horiz=FALSE,fill=mycol, border="black",cex=1.5)
+
+
