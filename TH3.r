0e6f066651da8edb07c02ed6503af25b9cdf42d9
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-04-06 08:50:34 +0700

    Update TH3.r

--- TH3.r
+++ TH3.r
@@ -5,8 +5,8 @@ data_2022<-mydata_ex2[,2];
  
 data_2023<- mydata_ex2[,3]
 mycol<-c("white","green","purple","violet","brown","red")
-my_pie1<-pie(data_2023,labels,radius=1,col=mycol,init.angle=90,main='tỉ trọng năm 2023')
-my_pie2<-pie(data_2022,labels,radius=1,col=mycol,init.angle=90,main='tỉ trọng năm 2022')
+pie(data_2023,labels=data_2023,radius=1,col=mycol,init.angle=90,main='tỉ trọng năm 2023')
+pie(data_2022,labels=data_2022,radius=1,col=mycol,init.angle=90,main='tỉ trọng năm 2022')
 
 par(mfrow = c(1,3)) ##chia bảng thành 3 phẦN
 
