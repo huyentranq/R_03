db0e81574ea52812d99771349b71887f1841ebf2
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-04-14 09:15:07 +0700

    Update TH4.R

--- TH4.R
+++ TH4.R
@@ -40,3 +40,46 @@ par(mfrow = c(1,2))
 
 boxplot ( TPSA , border = 'darkgreen ', horizontal=TRUE,col ='cornsilk ',
 ylim = c(0,20) , cex.axis =1)
+--------------------------EX5------------------------------------------------------------------
+
+setwd('D:/R_lab')
+data_ex5<- read.csv('lamphat.csv', header=T)
+
+count_inflation<- function(v,x)
+{
+	count<-0
+	n<-length(v)
+	for( i in(1:n)){
+		if(v[i]>x){
+		count<- count+1 }
+	}
+	return(count)
+}
+
+us<- count_inflation(data_ex5$US,5)
+uk<- count_inflation(data_ex5$UK,5)
+Japan<- count_inflation(data_ex5$Japan,5)
+Germany<- count_inflation(data_ex5$Germany,5)
+
+inflation_high <- c(us,uk,Japan,Germany)
+
+##trung bình, phương sai, trung vị và sai số chuẩn
+##US
+us <- data_ex5$US
+mean(us) ## trung binh
+var(us) ## phuong sai
+sd(us) ## do lech chuan
+
+
+
+
+
+
+
+
+
+
+
+
+
+
