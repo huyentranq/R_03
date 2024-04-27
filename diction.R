92ddb56bf32e4095bc33fd79f77f2355e5959f73
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-04-28 05:29:57 +0700

    Update diction.R

--- diction.R
+++ diction.R
@@ -8,5 +8,16 @@ byrow= T : chạy theo hàng
 v<- c(1,2,3,4,5,6,7,8,9)
 matrix(v,nrow=3,byrow=T)   
 
+## ma trận hoán vị : t()
+
+# tạo ra mộ ma trận 3 x 3 với tất cả phần tử là 0.
+ matrix(0, 3, 3) 
+
+# tạo ra mộ ma trận 3 x 3 với tất cả phần tử là 0.
+ A<- matrix(0, 3, 3) 
+
+ diag(A)<- 1 ## các phần tử trên đường chéo bằng 1
+
+
 
 
