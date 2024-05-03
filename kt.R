8293ee36329bc05be81ee5dcb3cea5649194f637
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-05-03 10:51:44 +0700

    Update kt.R

--- kt.R
+++ kt.R
@@ -8,3 +8,25 @@ barplot(rates, col= mycol, names.arg= rep(1:6),cex.name= 1,width=2, ylim= c(0,40
 par(mfrow= c(1,2))
 
 legend("left", legend= labels, horiz= FALSE, fill = mycol, cex= 1)
+
+
+----------------------------------
+setwd('D:/R_lab')
+ex4<- read.csv('SmartPhoneMarket2022-2023.csv',header=T)
+names<- ex4[,1]
+lb2022<- ex4[,2]
+lb2023<- ex4[,3]
+
+par(mfrow= c(1,1))
+my_plot<-barplot(rbind(lb1,lb2),beside=TRUE, col=c("blue","red"), names.arg= names, ylim= c(0,100))
+
+
+------ add text
+a<- rbind(lb2022, lb2023)
+n<- length(a)
+for(i in(1:n)){
+	text(x= my_plot[i], y=a[i]+5,labels = a[i], col= 'black', pos= 3, cex.name= 0.5)
+}
+
+
+my_plot[2]
