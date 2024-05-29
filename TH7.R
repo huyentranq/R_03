abf85599b11e37a8d5e472192f5febdb93f0b385
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-05-29 15:41:04 +0700

    Update TH7.R

--- TH7.R
+++ TH7.R
@@ -31,3 +31,51 @@ plot ( mydata_ts, plot.type ="single", lty = 1, col = mycol_ts , lwd =2.5,
 grid (8,8,col= 'darkgreen ', lty =3, lwd =1.2)
 dev.off()
 plot.new()
+----------------------------------------- Laptop cá nhân
+setwd('D:/R_lab')
+ex3<- read.csv('AirPassengers.csv',header= T)
+// chọn start, end theo cột dữ liệu
+mydata_ts<-ts(ex3[,-1],start=c(1949,1),end= c(1960,12),deltat= 1/12 )
+plot ( mydata_ts , col =" dodgerblue ", lwd =2.5, main ="",
+       col.axis ='orange2', col.lab ='darkgreen ', cex.axis =1.2)
+mycol_ts <- c(" dodgerblue ", "green4", "orange3", "purple3", "red3")
+#
+ts.plot ( mydata_ts, plot.type ="single", lty = 1, col = mycol_ts , lwd =2.5,
+       ylab =" Stock Price (usd )",cex.axis = 0.2)
+#####Bai 2
+## Loi giai: trang 5
+
+setwd('D:/R_lab')
+ex2<- read.csv('stock_price-US-nasdaq.csv',header= T)
+##
+
+## loc het cac du lieu NA
+ex2_filter<- subset(ex2, !is.na(ex2[, 3]))
+
+## Ðua data cua hai hang can dung vao mysubdata_ex2
+mysubdata_ex2<- data.frame(ex2_filter[1],ex2_filter[3],ex2_filter[5])
+
+##
+
+##
+x_vec <- seq( as.numeric(mydata 2$Year[1]) ,
+
+as.numeric(mydata2$Year[length ( mydata 2$ Year )]) , by=1)
+
+## ve cach don gian(trang 3)
+plot ( mysubdata_ex2[,2], type = 'l',col ='dodgerblue ', lwd =3,
+, axes = T )
+
+## them 1 duong du lieu len bieu do da ve
+lines(mysubdata_ex2[,3], type = 'l', col = 'red', lwd = 3)
+
+## Truc quan data
+plot( mysubdata_ex2 , col ="darkgreen", lwd =2.5, main ="",lty=1,
+       col.axis ='orange2', col.lab ='darkgreen ', cex.axis =1.2)
+
+plot (mysubdata_ex2, plot.type ="single", lty =1,col = mycol_ts , lwd =2.5)
+
+## 
+myplot <- barplot (mysubdata_ex2[,2] , border = NA , col = 'lightblue ',
+names.arg = mysubdata_ex2[,1] , cex.names =0.7)
+
