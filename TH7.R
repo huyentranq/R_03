88cea3c868a608633eedc86e452d75e78c536429
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-05-30 14:11:46 +0700

    Update TH7.R

--- TH7.R
+++ TH7.R
@@ -79,3 +79,37 @@ plot (mysubdata_ex2, plot.type ="single", lty =1,col = mycol_ts , lwd =2.5)
 myplot <- barplot (mysubdata_ex2[,2] , border = NA , col = 'lightblue ',
 names.arg = mysubdata_ex2[,1] , cex.names =0.7)
 
+----------------------------Bai 2
+#####Bai 2
+## Loi giai: trang 5
+
+setwd('D:/R_lab')
+ex2<- read.csv('stock_price-US-nasdaq.csv',header= T)
+##
+
+## loc het cac du lieu NA
+ex2_filter<- subset(ex2, !is.na(ex2[, 3]))
+
+## Ðua data cua hai hang can dung vao mysubdata_ex2
+mysubdata_ex2<- data.frame(ex2_filter[1],ex2_filter[3],ex2_filter[5])
+
+Date<- mysubdata_ex2[,1]
+## gioi han x y
+ylim_range <- range(mysubdata_ex2[,2])
+## Biểu đồ chính
+plot(mysubdata_ex2[,3], type = 'l', col = 'dodgerblue', lwd = 3,
+     ,axes= FALSE, xlim = c(0,22), ylim = ylim_range)
+
+## thêm 1 đường dữ liệu lên biểu đồ đã vẽ
+lines(mysubdata_ex2[,2], type = 'l', col = 'red', lwd = 3)
+
+## Vẽ thêm trục
+date_seq<- mysubdata_ex2[,1]
+current_data<- mysubdata_ex2[,3]
+##TRuc 0x
+axis(side=1, at = seq(1,22), labels = format(date_seq), las = 2)
+# Trục 0y
+axis(side=2, at = seq(0,600,30),labels = seq(0,600,30), las =1 )
+## las : hướng đặt của label
+ las=2 : hướng vuông góc với trục
+ las= 1: song song với trục
