0d8a26da998a4a7ed17abb0039e61a626ab4360e
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-05-31 09:23:51 +0700

    Update matlab.r

--- matlab.r
+++ matlab.r
@@ -1,3 +1,129 @@
+%%Họ tên: Nguyễn Thị Huyền Trang 
+%%mssv: 23280092
+
+%%% Bai 1: 
+clc
+clear all
+disp('Bài 1')
+syms a b c x y z
+f= (a*x+b*y+c*z)*sin(x*y*z)^-1
+fx=diff(f,x)
+fy=diff(f,y)
+fz=diff(f,z)
+fxx= diff(fx,x)
+fxy= diff(fx,y)
+fxz=diff(fx,z)
+fyy=diff(fy,y)
+fyx= diff(fy,x)
+fyz= diff(fy,z)
+fzz= diff(fz,z)
+fzx= diff(fz,x)
+fzy= diff(fz,y)
+
+%% Bài 2
+clc
+clear all
+disp('Bài 2')
+%% 2.a
+disp('2.a')
+f=x*sin(x+y);
+int(int(f,y,0,pi/6),x,0,pi/3)
+%% 2.b
+disp('2.b')
+syms x y
+y3=x-x^3;
+f= x^2+2*y;
+disp('nghiem cua phuong trinh hoanh do giao diem: ')
+x_tmp=solve(y3)
+int(int(f,y,x,x^3),x,0,1)
+
+
+%% 2.c
+disp('2.c')
+syms x
+y = sqrt(x);
+int(exp(x),x,1,4)
+
+%% 2d
+
+disp('2.d')
+syms x y t
+F=[x/sqrt(x^2+y^2) y/sqrt(x^2+y^2)];
+x=t; y=1+t^2;
+f=F(1)*diff(x)+F(2)*diff(y);
+int(f,t,-1,1)
+
+%% Bai 3
+clc
+clear all
+disp('Bài 3')
+clc
+clear all
+syms x y
+f=x*exp(-x*y)
+m=1000;
+n=1000;
+dx=(20-0)/m;
+dy=(10-0)/n;
+I=0
+for i= 1:m
+    for j= 1:n
+        x_tmp= 0+ i*dx;
+        y_tmp= 0+j*dy;
+        f_xy= x_tmp*exp(-x_tmp*y_tmp);
+        I=I+ f_xy*dx*dy;
+    end
+end
+disp('ket qua: ')
+I
+%% Bai 4
+clc
+clear all
+disp('4.a')
+syms y(x)
+ya = dsolve(diff(y,x) + y == 1, y(0) == 1)
+x_tmp=-5:0.1:5;
+y_sub= subs(ya,x,x_tmp);
+plot(x_tmp,y_sub)
+%4b
+disp('4.b')
+yb=dsolve((x^2+1)*diff(y,x) +3*x*(y-1)==0,y(0)==2)
+x_tmp= -5:0.1:5;
+y_sub= subs(yb,x_tmp);
+plot(x_tmp,y_sub)
+%4c
+
+disp('4.c')
+yc=dsolve(diff(y,x,2)-4*y==exp(x)*cos(x)+x^3,y(0)==1,subs(diff(y,x),x,0)==2)
+x_tmp= 5:0.1:10;
+y_sub= subs(yc,x_tmp);
+plot(x_tmp,y_sub)
+
+%% Bai 5
+clc
+clear all
+disp('Bài 5')
+a=-10:0.1:10;
+b=-10:0.1:20;
+[x,y]=meshgrid(a,b);
+z= 6*exp(-3*x.^2-y.^2)+x/2+y;
+plot3(x,y,z)
+
+
+%% Bai 6
+clc
+clear all
+disp('Bài 6')
+syms a b
+f= a*b- a^3/3;
+fx= diff(f,a);
+fy= diff(f,b);
+[x,y]=meshgrid(-5:0.5:5,-5:0.5:5);
+P= subs(fx,{a,b},{x,y});
+Q=subs(fy,{a,b},{x,y});
+quiver(x,y,P,Q)
+
+%% Bai 7
 disp('Bài 7')
 clc
 clear all
@@ -28,3 +154,9 @@ for(i=1:length(xc))
         end 
     end
 end
+
+
+
+
+
+
