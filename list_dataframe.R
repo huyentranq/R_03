58ddbfb6be0c10edaa255a5c253f1cb7da4a624e
HuyenTrang_ <155938605+huyentranq@users.noreply.github.com>
2025-03-13 09:10:48 +0700

    Update list_dataframe.R

--- list_dataframe.R
+++ list_dataframe.R
@@ -10,3 +10,23 @@ $scores
 [1] 2 5 6
 
 extract: mylist["name"]
+
+
+-------------
+trungbinh<- function(v){
+  return (sum(v)/length(v))
+}
+
+sbinh<- function(v){
+  tong<- 0
+  tb<- trungbinh(v)
+  for (x in v) {
+    tong <- tong+ (x-tb)^2
+  }
+  return (tong/(length(v)-1))
+}
+v<- c(2,4,5,1,7,9)
+sbinh(v)
+  s<- sqrt(sbinh(v))
+
+stderr_val<- s/sqrt(length(v))
